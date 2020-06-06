76009ffcb326ce7fc715a9b3a1f5469a69312648
fayucci <fyucci@gmail.com>
2020-06-06 16:35:00 +0200

    modific dataframe

--- server/app.py
+++ server/app.py
@@ -1,3 +1,4 @@
 from flask import Flask
 
 App = Flask(__name__, static_url_path='', static_folder='static')
+
--- server/cosmetics_list.py
+++ server/cosmetics_list.py
@@ -1,15 +1,40 @@
-from flask import jsonify
+from flask import jsonify, request
 import numpy as np 
 import pandas as pd
 
 
 
-df = pd.read_csv('datasets/cosmetics.csv')
+df = pd.read_csv('datasets/cosmetics.csv', dtype={
+	'combination': 'bool',
+	'dry': 'bool',
+	'normal':'bool',
+	'oily': 'bool',
+	'sensitive': 'bool'
+	}, names=['label', 'brand', 'name', 'price', 'rank', 'ingredients', 'combination', 'dry', 'normal', 'oily', 'sensitive'], header=0)
+
 
 df['id'] = df.index
 
-df_dict = df.to_dict('records')
-
 def cosmetics_list():
+	df_dict = df.loc[brand_filter()].to_dict('records')
 	return jsonify(df_dict)
-	
+
+
+#labels = df['label'].unique().tolist()
+#print(labels)
+# brands = df['brand'].unique().tolist()
+# print(brands)
+# max_price = df['price'].max()
+# print(max_price)
+# min_price = df['price'].min()
+# print(min_price)
+
+all = [True for i in df.iterrows()]
+
+def brand_filter():
+	brand_name = request.args.get('brand', '', str)
+	if brand_name:
+		return df['brand'] == brand_name
+	else:
+		return all
+
