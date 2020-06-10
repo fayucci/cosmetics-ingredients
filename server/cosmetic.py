75fb4ed87b6ba75a31eed0d884ebaf7c6db95409
fayucci <fyucci@gmail.com>
2020-06-10 20:58:48 +0200

    similars complet

--- server/cosmetic.py
+++ server/cosmetic.py
@@ -6,5 +6,5 @@ def cosmetic():
     row = df.iloc[id]
     ingredients = row['ingredients'].split(', ')
     row['ingredients'] = {ingredient: len(ingredients) - i for i, ingredient in enumerate(ingredients)}
-    row['similars'] = df[(df['x'] >= row['x']-1) & (df['x'] <= row['x']+1) & (df['y'] >= row['y']-1) & (df['y'] <= row['y']+1)][['id', 'name']].to_dict('records')
+    row['similars'] = df[(df['x'] >= row['x']-1) & (df['x'] <= row['x']+1) & (df['y'] >= row['y']-1) & (df['y'] <= row['y']+1)].drop(['ingredients', 'x', 'y'], axis=1).to_dict('records')
     return Response(row.to_json(), mimetype='application/json')
