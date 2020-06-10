0d6870e91e9895e181528809c1d51956b9c55773
fayucci <fyucci@gmail.com>
2020-06-10 21:47:56 +0200

    normalization ingre

--- server/cosmetic.py
+++ server/cosmetic.py
@@ -5,7 +5,8 @@ def cosmetic():
     id = request.args.get('id', type=int)
     row = df.loc[id]
     ingredients = row['ingredients'].split(', ')
-    ingredients_count = len(ingredients)
-    row['ingredients'] = [[ingredient, (ingredients_count - i) / ingredients_count] for i, ingredient in enumerate(ingredients)]
+    count = len(ingredients)
+    nor_fact = (count * (count + 1)) / 2
+    row['ingredients'] = [[ingredient, (count - i) / nor_fact] for i, ingredient in enumerate(ingredients)]
     row['similars'] = df[(df['x'] >= row['x']-1) & (df['x'] <= row['x']+1) & (df['y'] >= row['y']-1) & (df['y'] <= row['y']+1)].drop(['ingredients', 'x', 'y'], axis=1).to_dict('records')
     return Response(row.to_json(), mimetype='application/json')
