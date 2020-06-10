306afc323ba4741844862d6eed7c5d479a4bef23
fayucci <fyucci@gmail.com>
2020-06-10 22:42:15 +0200

    quit normalization

--- server/cosmetic.py
+++ server/cosmetic.py
@@ -6,6 +6,6 @@ def cosmetic():
     row = df.loc[id]
     ingredients = row['ingredients'].split(', ')
     ingredients_count = len(ingredients)
-    row['ingredients'] = [[ingredient, (ingredients_count - i) / ingredients_count] for i, ingredient in enumerate(ingredients)]
+    row['ingredients'] = [[ingredient, (ingredients_count - i)] for i, ingredient in enumerate(ingredients)]
     row['similars'] = df[(df['x'] >= row['x']-1) & (df['x'] <= row['x']+1) & (df['y'] >= row['y']-1) & (df['y'] <= row['y']+1)].drop(['ingredients', 'x', 'y'], axis=1).to_dict('records')
     return Response(row.to_json(), mimetype='application/json')
