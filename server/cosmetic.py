031b5d22f4154513f805d2368b485930ae45acc0
fayucci <fyucci@gmail.com>
2020-06-22 20:27:32 +0200

    similars without himselfs

--- server/cosmetic.py
+++ server/cosmetic.py
@@ -10,9 +10,12 @@ def cosmetic():
     row = df.loc[id]
     row['ingredients'] = row['ingredients'].split(', ')
     radius = 0.5
-    under_upper_limit = df['y'] <= row['y'] + radius
-    above_lower_limit = df['y'] >= row['y'] - radius
-    under_left_limit = df['x'] >= row['x'] - radius
-    above_right_limit = df['x'] <= row['x'] + radius
-    row['similars'] = df[under_upper_limit & above_lower_limit & under_left_limit & above_right_limit].drop(['ingredients', 'x', 'y'], axis=1).to_dict('records')
+    x = row['x']
+    y = row['y']
+    under_upper_limit = (df['y'] <= y + radius)
+    above_lower_limit = df['y'] >= y - radius
+    under_left_limit = df['x'] >= x - radius
+    above_right_limit = df['x'] <= x + radius
+    not_self = df['id'] != id
+    row['similars'] = df[under_upper_limit & above_lower_limit & under_left_limit & above_right_limit & not_self].drop(['ingredients', 'x', 'y'], axis=1).to_dict('records')
     return Response(row.to_json(), mimetype='application/json')
